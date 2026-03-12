517c810c8fdcc0cd618349368daa7a4d4662904f
Max Katz <maxkatz6@outlook.com>
2026-03-12 20:30:55 +0900

    Remove BabelLicense

--- nuke/Statics.cs
+++ nuke/Statics.cs
@@ -7,7 +7,6 @@ public static class Statics
 {
     public static AbsolutePath DefaultBuildCommon => NukeBuild.RootDirectory / "build-common";
     public static AbsolutePath AvaloniaStrongNameKey => DefaultBuildCommon / "avalonia.snk";
-    public static AbsolutePath BabelLicense => DefaultBuildCommon / "babel.license";
     public static AbsolutePath BabelRules => DefaultBuildCommon / "babel.rules";
     public static AbsolutePath Icon => DefaultBuildCommon / "branding" / "icon.png";
 }
